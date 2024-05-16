d3e8a1f7ea8a944dd59a4762de229b3482ccef57
HitomaruKonpaku <hunteratthenight@gmail.com>
2024-05-16 14:42:00 +0700

    Refactor main

--- apps/bot/src/main.ts
+++ apps/bot/src/main.ts
@@ -1,24 +1,7 @@
-import { NestFactory } from '@nestjs/core'
-import { Logger } from '@shared/logger/logger'
-import dotenv from 'dotenv'
+import { Main } from '@shared/base/base.main'
 import { BotModule } from './bot.module'
 
-async function bootstrap() {
-  dotenv.config({ path: '.env.bot' })
-  dotenv.config()
-
-  const logger = new Logger('MAIN', { timestamp: true })
-
-  const app = await NestFactory.create(BotModule, {
-    logger: new Logger(null, { timestamp: true }),
-  })
-
-  const port = process.env.PORT || 8080
-
-  await app.listen(port, () => {
-    const url = `http://localhost:${port}`
-    logger.log(`🚀 Server listening on ${url}`)
-  })
-}
-
-bootstrap()
+new Main(
+  'bot',
+  BotModule,
+).bootstrap()
--- apps/youtube-channel-crawler/src/main.ts
+++ apps/youtube-channel-crawler/src/main.ts
@@ -1,24 +1,7 @@
-import { NestFactory } from '@nestjs/core'
-import { Logger } from '@shared/logger/logger'
-import dotenv from 'dotenv'
+import { Main } from '@shared/base/base.main'
 import { YoutubeChannelCrawlerModule } from './youtube-channel-crawler.module'
 
-async function bootstrap() {
-  dotenv.config({ path: '.env.youtube-channel-crawler' })
-  dotenv.config()
-
-  const logger = new Logger('MAIN', { timestamp: true })
-
-  const app = await NestFactory.create(YoutubeChannelCrawlerModule, {
-    logger: new Logger(null, { timestamp: true }),
-  })
-
-  const port = process.env.PORT || 8080
-
-  await app.listen(port, () => {
-    const url = `http://localhost:${port}`
-    logger.log(`🚀 Server listening on ${url}`)
-  })
-}
-
-bootstrap()
+new Main(
+  'youtube-channel-crawler',
+  YoutubeChannelCrawlerModule,
+).bootstrap()
