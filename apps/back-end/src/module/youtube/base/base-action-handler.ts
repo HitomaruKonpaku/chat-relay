e6475726db5f7c9f25c2f86b1d82de7240d0de2d
HitomaruKonpaku <hunteratthenight@gmail.com>
2024-06-12 01:07:53 +0700

    Update queue config & add config to ignore old chat actions

--- apps/back-end/src/module/youtube/base/base-action-handler.ts
+++ apps/back-end/src/module/youtube/base/base-action-handler.ts
@@ -4,6 +4,7 @@ import { Track, TrackService } from '@app/track'
 import { UserFilter, UserFilterRepository, UserFilterType, UserSourceType } from '@app/user'
 import { YoutubeChatAction, YoutubeChatActionJobData, YoutubeChatUtil } from '@app/youtube'
 import { ModuleRef } from '@nestjs/core'
+import { NumberUtil } from '@shared/util/number.util'
 import { bold, inlineCode } from 'discord.js'
 import {
   AddBannerAction,
@@ -99,8 +100,16 @@ export abstract class BaseActionHandler<T1 extends HandlerAction, T2 extends Pro
   }
 
   protected async handleTrack(track: Track) {
+    const action = this.getProcessAction()
+
     if (!this.data.video.isLive && !track.allowReplay) {
       return
+    }
+    if (this.data.video.isLive && action.timestamp) {
+      const maxAge = NumberUtil.parse(process.env.YOUTUBE_ACTION_MAX_AGE, 2 * 60) * 1000
+      if (action.timestamp.getTime() + maxAge < Date.now()) {
+        return
+      }
     }
     if (this.data.video.isMembersOnly && !track.allowMemberChat) {
       return
@@ -118,7 +127,6 @@ export abstract class BaseActionHandler<T1 extends HandlerAction, T2 extends Pro
       }
     }
 
-    const action = this.getProcessAction()
     const message = stringify(action.message)
 
     if (this.userFilter?.type === UserFilterType.ALLOW) {
