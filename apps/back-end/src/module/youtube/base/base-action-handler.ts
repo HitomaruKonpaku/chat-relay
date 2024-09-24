a44b207a77bc8fb52586ec77d62a577cd7222e20
HitomaruKonpaku <hunteratthenight@gmail.com>
2024-09-24 19:22:09 +0700

    Add return

--- apps/back-end/src/module/youtube/base/base-action-handler.ts
+++ apps/back-end/src/module/youtube/base/base-action-handler.ts
@@ -120,7 +120,7 @@ export abstract class BaseActionHandler<T1 extends HandlerAction, T2 extends Pro
       const maxAge = (NumberUtil.parse(process.env.YOUTUBE_ACTION_MAX_AGE) || 3600) * 1000
       this.logger.warn(`ACTION_TIMESTAMP | ${JSON.stringify({ age, maxAge, valid: age > maxAge, action })}`)
       if (age > maxAge) {
-        // return
+        return
       }
     }
 
