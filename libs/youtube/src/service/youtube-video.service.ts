534f5bfad9d0e2f53cc437b6b42a3f15f30a62e4
HitomaruKonpaku <hunteratthenight@gmail.com>
2025-06-17 15:40:47 +0700

    Update masterchat & yt data handling

--- libs/youtube/src/service/youtube-video.service.ts
+++ libs/youtube/src/service/youtube-video.service.ts
@@ -18,6 +18,19 @@ export class YoutubeVideoService extends BaseService<YoutubeVideo> {
     super(repository)
   }
 
+  public async modified(id: string) {
+    await this.repository.repository.update({ id }, {
+      modifiedAt: Date.now(),
+    })
+  }
+
+  public async deactive(id: string) {
+    await this.repository.repository.update({ id }, {
+      isActive: false,
+      modifiedAt: Date.now(),
+    })
+  }
+
   public async updateMetadataMasterchat(id: string) {
     try {
       const mc = await Masterchat.init(id)
@@ -30,6 +43,7 @@ export class YoutubeVideoService extends BaseService<YoutubeVideo> {
         actualEnd: NumberUtil.fromDate(mc.videoMetadata?.publication?.endDate),
       }
       await this.repository.repository.update({ id }, data)
+      this.logger.log(`updateMetadataMasterchat | ${JSON.stringify({ id })}`)
     } catch (error) {
       this.logger.error(`updateMetadataMasterchat: ${error.message} | ${JSON.stringify({ id })}`)
     }
@@ -38,13 +52,28 @@ export class YoutubeVideoService extends BaseService<YoutubeVideo> {
   public async updateMetadataInnertube(id: string) {
     try {
       const { basic_info: info } = await this.innertubeService.getVideo(id)
+      if (!info.id) {
+        await this.deactive(id)
+        return
+      }
+
       const data: Partial<YoutubeVideo> = {
+        isActive: true,
         modifiedAt: Date.now(),
         isLiveContent: info.is_live_content,
       }
       await this.repository.repository.update({ id }, data)
+      this.logger.log(`updateMetadataInnertube | ${JSON.stringify({ id })}`)
     } catch (error) {
       this.logger.error(`updateMetadataInnertube: ${error.message} | ${JSON.stringify({ id })}`)
+      const reasons = [
+        'Video unavailable',
+        'This video has been removed for violating YouTube\'s Terms of Service',
+        'This video has been removed for violating YouTube\'s policy on nudity or sexual content',
+      ]
+      if (reasons.some((v) => v === error.info.reason)) {
+        await this.deactive(id)
+      }
     }
   }
 }
